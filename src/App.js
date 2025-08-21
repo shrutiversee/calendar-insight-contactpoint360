00d47c2c31ee4bb4cc17a8a10f784c3eb05b45db
Shruti-Agrahari <shrutiagrahari45@gmail.com>
2025-08-21 15:53:43 +0530

    added dummy data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ScheduleViewer from "./components/ScheduleViewer";
+import "./App.css";
 
-function App() {
+function MainApplication() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="MainApp">
+      <h1 style={{ color: "white", textAlign: "center" }}>🚀 Interactive Data Calendar Hub</h1>
+      <ScheduleViewer />
     </div>
   );
 }
 
-export default App;
+export default MainApplication;
